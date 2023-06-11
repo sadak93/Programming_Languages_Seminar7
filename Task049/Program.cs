071b01f34aaf01534ca4c866058da596a511bb5f
Sadak <s.sadak02@gmail.com>
2023-06-11 09:10:07 +0600

    Двумерный массив. Квадрат значений с нечетными индексами

---
+++ Task049/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса нечётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть 
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = new Random().Next(0,10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j],3}    ");
+        System.Console.WriteLine();
+        }
+}
+
+void DoubleNum(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (i%2==1 && j%2==1)
+                array[i,j]*=array[i,j];
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows, cols];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+DoubleNum(array);
+PrintArray(array);
