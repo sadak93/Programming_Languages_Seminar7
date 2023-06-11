425b03b6563a584bda678369b458e3a4c6c68d4b
Sadak <s.sadak02@gmail.com>
2023-06-11 09:41:39 +0600

    Двумерный массив

---
+++ Task046/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 46: Задайте двумерный массив размером rows×cols, заполненный 
+//случайными целыми числами.
+// rows = 3, cols = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = new Random().Next(-30,31);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j],3}    ");
+        System.Console.WriteLine();
+        }
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows, cols];
+FillArray(array);
+PrintArray(array);
+
